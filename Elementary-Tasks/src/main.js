0b01b5e5328118c3677322231c206c187ec35128
Ertugrul Alina <boykoalina03@gmail.com>
2020-11-13 11:43:03 +0200

    added palindrom

--- Elementary-Tasks/src/main.js
+++ Elementary-Tasks/src/main.js
@@ -1,54 +1,65 @@
 // 1.	Шахматная доска
 // Вывести шахматную доску с заданными размерами высоты и ширины, по принципу:
 // *  *  *  *  *  *
-//   *  *  *  *  *  *  
+//   *  *  *  *  *  *
 // *  *  *  *  *  *
 //   *  *  *  *  *  *
 
 // Входные параметры: длина, ширина, символ для отображения.
 // Выход: строка с представлением шахматной доски
 
-function desk(width, height, symbol){
-    let column=""
-    
-    for(let i=0; i<height; i++){        
-        let row= "";
-        for(let i=0; i<width; i++){
-        row+=(symbol+" ");
-        }
-        if (i%2!=0){
-        column+=" "+row+"\n";
-    }else{column+=row+"\n";}
+function drawDesk(width, height, symbol) {
+  let column = "";
+
+  for (let i = 0; i < height; i++) {
+    let row = "";
+    for (let i = 0; i < width; i++) {
+      row += symbol + " ";
     }
-    return column
+    if (i % 2 != 0) {
+      column += " " + row + "\n";
+    } else {
+      column += row + "\n";
+    }
+  }
+  return column;
 }
-console.log(desk(10,5,"*"))
+console.log(drawDesk(10, 5, "*"));
 
 // 2.	Анализ конвертов
-// Есть два конверта со сторонами (a,b) и (c,d). Требуется определить, можно ли один конверт вложить в другой. Программа должна обрабатывать ввод чисел с плавающей точкой. 
+// Есть два конверта со сторонами (a,b) и (c,d). Требуется определить, можно ли один конверт вложить в другой. Программа должна обрабатывать ввод чисел с плавающей точкой.
 
-// Входные параметры: объекты конверт1 и конверт2 
+// Входные параметры: объекты конверт1 и конверт2
 // Выход: номер конверта, если вложение возможно, 0 – если вложение невозможно.
 
 let letter1 = {
-    number:1,
-    width:5,
-    height:5
+  number: 1,
+  width: 3,
+  height: 3,
 };
 
 let letter2 = {
-    number:2,
-    width:4.5,
-    height:5
-}
-
-if(letter1.width>letter2.width && letter1.height>letter2.height){
-    alert(letter1.number)
-}else if(letter2.width>letter1.width && letter2.height>letter1.height){
-    alert(letter2.number)
-}else {
-    alert("0")
+  number: 2,
+  width: 4.5,
+  height: 4,
 };
+
+function compareLetter(firstLetter, secondLetter) {
+  if (
+    firstLetter.width > secondLetter.width &&
+    firstLetter.height > secondLetter.height
+  ) {
+    console.log("1");
+  } else if (
+    secondLetter.width > firstLetter.width &&
+    secondLetter.height > firstLetter.height
+  ) {
+    console.log("2");
+  } else {
+    console.log("0");
+  }
+}
+compareLetter(letter1, letter2);
 
 // 3.	Сортировка треугольников
 // Вывести треугольники в порядке убывания их площади.
@@ -58,9 +69,9 @@ if(letter1.width>letter2.width && letter1.height>letter2.height){
 
 // Примечание:
 // •	Расчёт площади треугольника должен производится по формуле Герона.
-// •	Каждый треугольник определяется именами вершин и длинами его сторон. 
+// •	Каждый треугольник определяется именами вершин и длинами его сторон.
 // •	Приложение должно обрабатывать ввод чисел с плавающей точкой:
-// { 
+// {
 // vertices: ‘ABC’,
 // a: 10,
 // b: 20,
@@ -73,15 +84,41 @@ if(letter1.width>letter2.width && letter1.height>letter2.height){
 // Входные параметры: число
 // Выход: извлечённый из числа палиндром либо 0, если извлечение не удалось.
 
-function palindrom(numberIsString){
-   let isPalindrom=""
-    for(let i=0; i<numberIsString.length; i++){
-        if(numberIsString[i]==numberIsString.split('').reverse().join('')[i]){
-            isPalindrom+=+numberIsString[i]
-        }
+function palindrom(number) {
+  function iter(inexI, indexJ) {
+    if (number[inexI] === number[indexJ]) {
+      return iter(inexI - 1, indexJ + 1);
     }
-    return isPalindrom
-};
+    return number.slice(inexI + 1, indexJ);
+  }
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] == number[i + 1]) {
+      return iter(i - 1, i + 2);
+      break;
+    }
+  }
+  return 0;
+}
 
 console.log(palindrom("1234437"));
 
+// Реализуйте тело функции smallestDivisor, используя итеративный процесс. Эта функция должна находить наименьший делитель заданного числа.
+
+// Доп. условия: число, передаваемое в функцию, больше нуля; делитель должен быть больше единицы, за исключением случая, когда аргументом является единица (наименьшим делителем которой является также единица).
+
+// Например, наименьший делитель числа 15 это 3.
+
+// const smallestDivisor = (num) => {
+//     const iter = (counter, acc) =>{
+//         if(counter === 1){
+//             return 1
+//         }
+//         if (counter % acc === 0){
+//             return acc;
+//         }
+//         return iter(counter,acc+1)
+//     };
+//     return iter(num,2);
+//    };
+
+//    console.log(smallestDivisor(1))
