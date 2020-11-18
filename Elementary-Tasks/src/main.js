45375a6ad906af7c6c747fa2455a381dff77c883
Ertugrul Alina <boykoalina03@gmail.com>
2020-11-18 15:09:24 +0200

    added 6,7

--- Elementary-Tasks/src/main.js
+++ Elementary-Tasks/src/main.js
@@ -123,6 +123,8 @@ console.log(getPalindrom("1234437"));
 
 //    console.log(smallestDivisor(1))
 
+
+// 3.	Сортировка треугольников
 // Вывести треугольники в порядке убывания их площади.
 
 // Входные параметры: массив объектов треугольник
@@ -132,23 +134,38 @@ console.log(getPalindrom("1234437"));
 // •	Расчёт площади треугольника должен производится по формуле Герона.
 // •	Каждый треугольник определяется именами вершин и длинами его сторон.
 // •	Приложение должно обрабатывать ввод чисел с плавающей точкой:
+// {
+// vertices: ‘ABC’,
+// a: 10,
+// b: 20,
+// c: 22.36
+// }
 
 let compareArea = (array) => {
   let TrianglesList = [];
   for (value of array) {
-    let item = value.name;
-    TrianglesList[item] = value.getSquare();
+     let item = value.name;
+      TrianglesList[item] = value.square;
+    }
+    TrianglesList.sort((a, b) => b[1] - a[1]);
+    return TrianglesList;
   }
-  TrianglesList.sort((a, b) => b[0] - a[0]);
-  return TrianglesList;
-};
-
+  // console.log(array[0].square)
+  // let TrianglesList = [];
+  // for (value of array) {
+  //   let item = value.name;
+  //   TrianglesList[item] = value.square;
+  // }
+  // TrianglesList.sort((a, b) => b[1] - a[1]);
+//   return TrianglesList;
+// };
 class Triangle {
   constructor(name, a, b, c) {
     this.name = name;
     this.a = a;
     this.b = b;
     this.c = c;
+    this.square=this.getSquare();
   }
   getSquare() {
     return (
@@ -158,6 +175,7 @@ class Triangle {
       ) / 4
     );
   }
+  
 }
 
 let triangl1 = new Triangle("Triangl1", 10, 20, 22.36);
@@ -166,3 +184,80 @@ let triangl3 = new Triangle("Triangl3", 20, 30, 40);
 
 let TrianglesArray = [triangl1, triangl2, triangl3];
 console.log(compareArea(TrianglesArray));
+
+// 6.	Числовая последовательность
+// Вывести через запятую ряд длиной n, состоящий из натуральных чисел, квадрат которых не меньше заданного m.
+
+// Входные параметры: длина и значение минимального квадрата
+// Выход: строка с рядом чисел
+
+function getNaturalNumbrsStr(n, m){
+  let i = 2;
+  let strNaturalNumbrs=[];
+  let j=0
+  while(i<=1000){
+    if (i**2>=m){
+      strNaturalNumbrs[j] =i;
+      j+=1
+    }
+    if (strNaturalNumbrs.length >=n){
+      break;
+    }
+    i+=1
+
+  }
+  return strNaturalNumbrs
+}
+
+  console.log(getNaturalNumbrsStr(5, 30));
+
+//   7.	Ряд Фибоначчи для диапазона
+// Вывести все числа Фибоначчи, которые удовлетворяют переданному в функцию ограничению: находятся в указанном диапазоне, либо имеют указанную длину.
+
+// Входные параметры: объект context с полями min и max, либо с полем length 
+// Выход: массив чисел
+
+function getFib1(n){
+  let a = 1;
+  let b = 1;
+  let j = 2;
+  let fibStr=[1, 1];
+  for(let i=3; i<=n; i++){
+    let c=a+b;
+    fibStr[j]=c;
+    j+=1
+    a=b;
+    b=c;
+  }
+  return fibStr
+}
+function getFib2(min, max){
+  let a = 0;
+  let b = 1;
+  let j = 0;
+  let fibStr=[];
+  for(let i=2; b<=max; i++){
+    let c=a+b;
+    if (b>=min && b<=max){
+      fibStr[j]=b; 
+      j+=1 
+    };
+    a=b;
+    b=c;
+  }
+  return fibStr
+};
+function getFib(obj){
+  if (obj.length){
+    return getFib1(obj.length)
+  };
+  return getFib2(obj.min, obj.max)
+};
+
+
+let context ={
+  min:3,
+  max:100,
+  length
+};
+console.log(getFib(context))
